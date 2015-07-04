29c6af11316979d397a6cbc1575b1ad8c86990bf
Ubuntu <ubuntu@ip-172-31-3-137.ap-southeast-2.compute.internal>
2015-07-04 03:57:26 +0000

    Initial F3 commit

---
+++ api/index.php
@@ -0,0 +1,109 @@
+<?php
+
+$BASEURL = "http://54.153.189.148/";
+$CLIENTURL = $BASEURL."sims/web/";
+$APIURL = $BASEURL."sims/api/";
+
+// Kickstart the framework
+$f3=require('lib/base.php');
+
+$f3->set('DEBUG',1);
+if ((float)PCRE_VERSION<7.9)
+	trigger_error('PCRE version is out of date');
+
+$db=new \DB\SQL('mysql:host=localhost;port=3306;dbname=sims','root','root');
+
+// Load configuration
+$f3->config('config.ini');
+
+$f3->route('GET /',
+	function($f3) {
+		$classes=array(
+			'Base'=>
+				array(
+					'hash',
+					'json',
+					'session'
+				),
+			'Cache'=>
+				array(
+					'apc',
+					'memcache',
+					'wincache',
+					'xcache'
+				),
+			'DB\SQL'=>
+				array(
+					'pdo',
+					'pdo_dblib',
+					'pdo_mssql',
+					'pdo_mysql',
+					'pdo_odbc',
+					'pdo_pgsql',
+					'pdo_sqlite',
+					'pdo_sqlsrv'
+				),
+			'DB\Jig'=>
+				array('json'),
+			'DB\Mongo'=>
+				array(
+					'json',
+					'mongo'
+				),
+			'Auth'=>
+				array('ldap','pdo'),
+			'Bcrypt'=>
+				array(
+					'mcrypt',
+					'openssl'
+				),
+			'Image'=>
+				array('gd'),
+			'Lexicon'=>
+				array('iconv'),
+			'SMTP'=>
+				array('openssl'),
+			'Web'=>
+				array('curl','openssl','simplexml'),
+			'Web\Geo'=>
+				array('geoip','json'),
+			'Web\OpenID'=>
+				array('json','simplexml'),
+			'Web\Pingback'=>
+				array('dom','xmlrpc')
+		);
+		$f3->set('classes',$classes);
+		$f3->set('content','welcome.htm');
+		echo View::instance()->render('layout.htm');
+	}
+);
+
+$f3->route('GET /userref',
+	function($f3) {
+		$f3->set('content','userref.htm');
+		echo View::instance()->render('layout.htm');
+	}
+);
+
+$f3->route('GET /services',
+    function()
+    {
+        global $db, $f3;
+        $name = $f3->get('POST.name');
+        $rows=$db->exec('SELECT DISTINCT category FROM points');
+        echo json_encode($rows);
+    }
+);
+
+
+$f3->route('GET /trees',
+    function() {
+        global $db, $f3;
+        $sql = "Select *
+                From trees";
+        $rows=$db->exec($sql);
+        echo json_encode($rows);
+    }
+);
+
+$f3->run();
