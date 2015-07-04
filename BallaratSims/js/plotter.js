726e59cfd265007360b20e7337ea5e233d0b1f0e
Robert Layton <robertlayton@gmail.com>
2015-07-04 21:25:04 +1000

    Able to select from various services or transports and the map updates itself.
    A weird javascript bug pops up, but the map still draws. Crossing fingers that this doesn't bite us later

--- BallaratSims/js/plotter.js
+++ BallaratSims/js/plotter.js
@@ -1,22 +1,29 @@
 
 var points = [];
-points.push(new google.maps.LatLng(-37.5500, 143.8500));
-points.push(new google.maps.LatLng(-37.5400, 143.8500));
-points.push(new google.maps.LatLng(-37.5600, 143.8000));
+//points.push(new google.maps.LatLng(-37.5500, 143.8500));
+//points.push(new google.maps.LatLng(-37.5400, 143.8500));
+//points.push(new google.maps.LatLng(-37.5600, 143.8000));
 
 var map = null;
 var heatmap = null;
 
+var service_type = 'EDU';
+var transport_type = 'WALK';
+
 var gradient = [
     'rgba(0, 255, 255, 0)',
     'rgba(255, 0, 0, 1)'
-]
+];
+
+var distances_travelled = {'WALK': 0.00833, "RIDE": 0.02583, "DRIVE": 0.06666};
+$WALKDISTANCE = 833; //Average distance a person can walk in 10 minutes (m)
+$RIDEDISTANCE = 2583; //Average distance a person can ride in 10 minutes (m)
+$DRIVEDISTANCE = 6666; //Distance that can be driven in 10 minutes based on a 40km/h average speed (m)
 
 
 function plot_points(map){
     points.map( function(point) {
         // Draw marker
-        console.log(point);
         var marker = new google.maps.Marker({
             position: point,
             map: map,
@@ -38,12 +45,47 @@ function plot_points(map){
     });
 }
 
-function heat_map(map){
+function heat_map(){
     console.log("Starting heatmap");
+    var data = [];
+    points.map( function(point) {
+        data.push({lat: point.lat(), lng: point.lng(), count:5});
+    });
+
+    var heatmap_data = {
+        max: 100,
+        data: data
+    };
+
+    heatmap.setData(heatmap_data);
+}
+
+function update_map(result){
+    points = [];
+    result.map(function(row){
+        points.push(new google.maps.LatLng(row['lat'], row['lon']))
+    });
+
+    console.log("Setting size: " + distances_travelled[transport_type]);
+    heatmap.configure({radius: distances_travelled[transport_type]});
+    heat_map();
+}
+
+
+function initialize(result) {
+    points = [];
+    result.map(function(row){
+        points.push(new google.maps.LatLng(row['lat'], row['lon']))
+    });
+    var mapOptions = {
+        zoom: 12,
+        center: new google.maps.LatLng(-37.5500, 143.8500)
+    };
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
     heatmap = new HeatmapOverlay(map,
         {
             // radius should be small ONLY if scaleRadius is true (or small radius is intended)
-            "radius": 0.01,
+            "radius": distances_travelled[transport_type],
             "maxOpacity": 0.8,
             // scales the radius based on map zoom
             "scaleRadius": true,
@@ -66,31 +108,30 @@ function heat_map(map){
             }
         }
     );
+    //plot_points(map);
+    heat_map();
+}
 
-    var data = [];
-    points.map( function(point) {
-        data.push({lat: point.lat(), lng: point.lng(), count:5});
-        console.log(data);
+
+function load_initial_points(){
+    var data_url = "http://planr.ballarathackerspace.org.au/sims/api/services/EDU";
+    $.ajax(data_url, {
+        success: initialize,
+        dataType: "json"
     });
-
-    var heatmap_data = {
-        max: 8,
-        data: data
-    };
-
-    heatmap.setData(heatmap_data);
 }
 
-function initialize() {
-    var mapOptions = {
-        zoom: 10,
-        center: points[0]
-    }
-    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-    plot_points(map);
-    heat_map(map);
+google.maps.event.addDomListener(window, 'load', load_initial_points);
+
+
+function update_city_heatmap(){
+    // 1 get service type
+    service_type = $("#service_type").val();
+    // 2 service type
+    transport_type = $("#transport_type").val();
+    var data_url = "http://planr.ballarathackerspace.org.au/sims/api/services/" + service_type;
+    $.ajax(data_url, {
+        success: initialize,
+        dataType: 'json'
+    })
 }
-
-google.maps.event.addDomListener(window, 'load', initialize);
-
-
